580840f36781b691174fccca384847cbe43cb866
PizzaCrust <pizzacrustcoder@gmail.com>
2017-09-14 16:26:07 +0700

    add add/removal in support with the new sls feature

--- src/SLC/Script.lua
+++ src/SLC/Script.lua
@@ -14,16 +14,37 @@ local httpService = game:GetService("HttpService")
 
 local link = url .. "/registerserver?code=" .. authCode .. "&instance_id=" .. instanceId .. "&place_id=" .. placeId .. "&regpc=" .. currentPlayers
 
-game:BindToClose(function()
-    print("got the game is closing")
-    local closeLink = script.Parent.ServerListServerURL.Value .. "/delserver?code=" .. script.Parent.AuthenticationCode.Value .. "&instance_id=" .. game.JobId
-    print("sending del server")
-    local potato = game:GetService("HttpService")
-    potato:PostAsync(closeLink, "")
-    print("sent")
+game:BindToClose(function() 
+	print("got the game is closing")
+	local closeLink = script.Parent.ServerListServerURL.Value .. "/delserver?code=" .. script.Parent.AuthenticationCode.Value .. "&instance_id=" .. game.JobId
+	print("sending del server")
+	local potato = game:GetService("HttpService")
+	potato:PostAsync(closeLink, "")
+	print("sent")
 end)
 
-print("sending code")
+local function onAdded(player) 
+	print("detected person, sending id to server")
+	local link = script.Parent.ServerListServerURL.Value .. "/setplayercount?code=" .. script.Parent.AuthenticationCode.Value .. "&instance_id=" .. game.JobId .. "&number=1"	
+	print("sending id")
+	local potato = game:GetService("HttpService")
+	potato:PostAsync(link, "")
+	print("i sent it lol")
+end
+
+local function onRemove(player)
+	print("detected person removal, sending id to server")
+	local link = script.Parent.ServerListServerURL.Value .. "/setplayercount?code=" .. script.Parent.AuthenticationCode.Value .. "&instance_id=" .. game.JobId .. "&number=-1"	
+	print("sending id")
+	local potato = game:GetService("HttpService")
+	potato:PostAsync(link, "")
+	print("i sent it lol")
+end
+
+game.Players.PlayerAdded:Connect(onAdded)
+game.Players.PlayerRemoving:Connect(onRemove)
+
+print("sending code") 
 --print(link)
 httpService:PostAsync(link, "")
 print("code sent to server")
